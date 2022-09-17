e40746276d7c60fdc0a7fab5e3a73fec183dd314
Tariq A <tachorzyad@gmail.com>
2022-09-17 13:22:05 -0500

    Database changes

--- src/ReactionRole.ts
+++
@@ -1,9 +0,0 @@
-import { Client, EmbedBuilder, Routes } from "discord.js";
-import { commands } from "./Commands";
-
-export default(embed: EmbedBuilder): void => {
-    
-    
-
-
-}
--- src/models/Event.ts
+++
@@ -1,5 +0,0 @@
-import { ObjectId } from "mongodb";
-
-export default class Event {
-    constructor(public title?: string, public date?: Date, public userIds?: string[], public id?: ObjectId) {}
-}
