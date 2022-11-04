7fec9a5d3067497ea4931cd7a0626c1c41c23c37
Mihir Sahu <2002mihir@gmail.com>
2022-11-04 02:59:28 -0500

    Add function to scrape UH website

---
+++ src/commands/classScraper.ts
@@ -0,0 +1,28 @@
+import fetch from 'cross-fetch';
+import fs from 'fs';
+
+const classScraper = async () => {
+    let data = await fetch("https://uh.libcal.com/widget/equipment/ajax/display", {
+            "headers": {
+            "accept": "text/html, */*; q=0.01",
+            "accept-language": "en-US,en;q=0.5",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "sec-gpc": "1",
+            "x-requested-with": "XMLHttpRequest",
+            "Referer": "https://uh.libcal.com/widget/equipment?gid=6713&eid=0&iid=4043",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            "body": "iid=4043&gid=6713&capacity=0&date=2022-11-04&slots=",
+            "method": "POST"
+    });
+
+    //fs.writeFile('./scrapedData.txt', await data.text(), { flag: 'w+' }, err => { console.log(err) });
+    //console.log(await data.text());
+
+    return data;
+}
+
+export default classScraper;
